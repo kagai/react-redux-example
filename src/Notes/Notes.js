848aefd12a7322f43e6277b2ad406338624bcbdc
kagai <shaddy.mwangi@gmail.com>
2019-09-13 16:15:19 +0300

    first commit

---
+++ src/Notes/Notes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import NotesForm from './NotesForm';
+import AllNotes from './AllNotes';
+
+ class Notes extends Component {
+    render() {
+        return (
+          <React.Fragment>
+            <h1>React Redux Notes</h1>
+            <NotesForm/>
+            <hr/>
+            <AllNotes/>
+          </React.Fragment>
+        )
+    }
+}
+export default Notes;
